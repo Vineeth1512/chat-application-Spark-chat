ff7141d6e69eacbafd948fde0e531c1a7a5ce3f3
Vineeth1512 <114067110+Vineeth1512@users.noreply.github.com>
2024-08-02 22:45:02 +0530

    added welcome chat -app line

--- index.js
+++ index.js
@@ -24,3 +24,8 @@ server.listen(process.env.PORT, () => {
   //database connection
   databaseConnect();
 });
+app.get("/", (req, res) => {
+  return res.status(200).json({
+    message: "Welcome to SparkChat Application.",
+  });
+});

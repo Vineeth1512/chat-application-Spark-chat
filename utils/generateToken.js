105d54cf8cfefa0f30dfeda5c8567408427f462e
Vineeth1512 <114067110+Vineeth1512@users.noreply.github.com>
2024-08-03 11:05:29 +0530

    addedcNODE_ENV line

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -8,7 +8,7 @@ const generateTokenSetCookie = (userId, res) => {
     maxAge: 1 * 24 * 60 * 60 * 1000, //MS
     httpOnly: true,
     sameSite: "strict",
-    secure: true,
+    secure: process.env.NODE_ENV !== "development",
   });
 };
 export default generateTokenSetCookie;
